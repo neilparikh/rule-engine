d6de181e54cbdac4ac74ff41b23eb8a5e9842341
Neil Parikh <neil@tophat.com>
2016-11-12 10:29:57 -0500

    remove commented code

--- src/Types.hs
+++ src/Types.hs
@@ -1,4 +1,3 @@
--- {-# Language GADTs, DataKinds, KindSignatures, ExistentialQuantification, StandaloneDeriving #-}
 {-# Language GeneralizedNewtypeDeriving #-}
 module Types where
 
@@ -6,14 +5,6 @@ import Text.Parsec (Parsec)
 import Data.String (IsString)
 
 type Parser a = Parsec String () a
-
--- data Rule = forall a. Rule (Condition a) Action
--- deriving instance Show Rule
--- data ConditionType = Comp | Cmp deriving Show
--- data Condition (a :: ConditionType) where
---     Compound :: Conjunction -> Condition a -> Condition b -> Condition 'Comp
---     Compare  :: Predicate -> Expr -> Expr -> Condition 'Cmp
--- deriving instance Show (Condition (a :: ConditionType))
 
 data Rule = Rule Condition Action deriving Eq
 
