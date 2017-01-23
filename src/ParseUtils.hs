772b414a6cb39bbc25908d244d414370a15d9601
Neil Parikh <neil.parikh@careguide.com>
2017-01-22 21:59:18 -0500

    Some cleanup
    - applicative syntax in ParseUtils
    - some helper parsers to remove duplication

--- src/ParseUtils.hs
+++ src/ParseUtils.hs
@@ -2,14 +2,14 @@ module ParseUtils where
 import Text.Parsec (string, char, spaces)
 import Types (Parser)
 
+wrapWith :: Parser a -> Parser b -> Parser b
+wrapWith wrapper subParser = wrapper *> subParser <* wrapper
+
+wrapWithSpaces :: Parser a -> Parser a
+wrapWithSpaces = wrapWith spaces
+
 constString :: String -> a -> Parser a
-constString s f = string s >> return f
+constString s f = string s *> return f
 
 parens :: Parser a -> Parser a
-parens subParser = do
-    char '('
-    spaces
-    a <- subParser
-    spaces
-    char ')'
-    return a
+parens subParser = char '(' *> wrapWithSpaces subParser <* char ')'
